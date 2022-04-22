6711331d0d58b5b9e4ea7d28509a23acf5f8454a
yanglae <yanglae@hanmail.net>
2022-04-22 20:24:33 +0900

    mobile

--- resources/js/common.js
+++ resources/js/common.js
@@ -1,20 +1,20 @@
 $(function() {
-    $(window).on('load', function() {
-        mainVertical();
+    $(window).on("resize load", function () {
+        //mainVertical();
     });
 	mainVertical();
 	gnbMenu();
-	pointer();	
+	pointer();
+	moNav();
 });
 
-
 // 메인 - 서비스소개
-function mainVertical() {    
+function mainVertical() {
     var mainVertical = new Swiper('.fullscreen', {
         direction: "vertical",
         effect: "slide",
         slidesPerView: 'auto',
-        speed: 1000,
+        speed: 800,
         allowTouchMove:false,
         simulateTouch:false,
         touchStartPreventDefault:false,
@@ -42,12 +42,12 @@ function mainVertical() {
 				
 				var footHeight = $(".footer_section").outerHeight();
 				if($(".footer_section").hasClass("swiper-slide-visible")){
-					$(".btn_top_wrap").css("position","fixed").css("bottom",footHeight + 80).addClass('on');
+					$(".btn_top_wrap").css("position","fixed").css("bottom",footHeight + 60).addClass('on');
 				}else{
 					$(".btn_top_wrap").css("position","fixed").css("bottom","80px").removeClass('on');
 				}
 			},
-			transitionEnd: function(){				
+			transitionEnd: function(){
 			},
 		},
 
@@ -67,13 +67,28 @@ function mainVertical() {
 	
 }
 
-// 상단메뉴
+// 상단메뉴 페이지인식
 function gnbMenu(depth1) {
     $('.gnb li').eq(depth1-1).find('> a').addClass('on');
 }
 
+// 모바일 mo_nav_open
+function moNav() {
+	// $('.btn_menu_open').on('click',function(){
+	// 	var href = $(this).attr("href");
+	// 	$('.mo_nav_wrap' + href).addClass('on');
+	// 	$('body').css({'height':$(window).height(), 'overflow':'hidden'});
+	// });
+	// $('.mo_nav_wrap .btn_close').on('click', function(e){
+	// 	e.preventDefault();
+	// 	$('.mo_nav_wrap').removeClass('on');
+	// 	$('body').css({'height':$(window).height(), 'overflow':'auto'});
+	// });
+}
+
+
 //마우스 포인터
-function pointer() {	
+function pointer() {
 	var pointSize = $(".pointer").width()/2;
 	$(window).on("mousemove", function(e){
 		$('.pointer').css("top", e.pageY-pointSize);
